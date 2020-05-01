05b2827f40b413427758d0b3f306a1139552577e
CptFrikadel <adkupper@gmail.com>
2020-05-01 23:49:55 +0200

    Date parsing

--- IGCReader.cpp
+++ IGCReader.cpp
@@ -26,34 +26,50 @@ void IGCReader::readIGC(){
         if (igc_file.is_open()){
             std::cout << "Parsing: " << file_name << std::endl;
 
-
-            std::string line;
-            std::string time_line;
-            bool found_takeoff = false;
-
-            // Parse file line by line
-            while(std::getline(igc_file, line)){
-
-                if (line[0] == 'B'){
-                    // line is a position report
-                    time_line = line;
-
-                    if (!found_takeoff){
-                        // First position, set takeoff time
-
-                        // TODO: add altitude condition
-
-                        std::cout << line << std::endl;
-                        flight_data.takeoff_time = parseTime(line);
-                        found_takeoff = true;
-                    }
-                }
-            }
-            flight_data.landing_time = parseTime(time_line);
-
         } else {
             std::cerr << "Unable to open file: " << file_name << std::endl;
+            return;
         }
+
+        std::string line;
+        std::string time_line;
+        std::string date;
+        bool found_takeoff = false;
+
+        // Parse file line by line
+        while(std::getline(igc_file, line)){
+
+            if (line[0] == 'B'){
+                // line is a position report
+                time_line = line;
+
+                if (!found_takeoff){
+                    // First position, set takeoff time
+
+                    // TODO: add altitude condition
+
+                    std::cout << line << std::endl;
+                    flight_data.takeoff_time = parseTime(line);
+                    found_takeoff = true;
+                }
+
+            } else if (line[0] == 'H'){
+                // Line is a header record
+
+                if (line[1] == 'F' || line[1] == 'P'){
+                    // source element, nex three bytes are a mnemonic
+                    if (line.substr(2,3) == "DTE"){
+                        // Line is a date record
+                        date = line.substr(5,6);
+                    }
+
+                }
+
+            }
+        }
+        flight_data.landing_time = parseTime(time_line);
+        std::cout << date << std::endl;
+
 
         igc_file.close();
 }
