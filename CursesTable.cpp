f74dc4c79c04f5433bff21f6423e607599aa41f8
CptFrikadel <adkupper@gmail.com>
2020-05-13 00:26:12 +0200

    Added dimensions tuple

--- CursesTable.cpp
+++ CursesTable.cpp
@@ -4,7 +4,14 @@
 
 CursesTable::CursesTable(const unsigned int _num_columns) : num_columns(_num_columns){
 
+    dimensions = std::make_tuple(COLS, LINES);
 }
+
+CursesTable::CursesTable(const unsigned int _num_columns, const int width, const int height) :
+    num_columns(_num_columns){
+
+        dimensions = std::make_tuple(width, height);
+    }
 
 void CursesTable::draw(){
 
--- CursesTable.hpp
+++ CursesTable.hpp
@@ -7,11 +7,13 @@
 
 #include <vector>
 #include <string>
+#include <tuple>
 
 class CursesTable {
 
 public:
     CursesTable(const unsigned int num_columns);
+    CursesTable(const unsigned int num_columns, const int width, const int height);
     void addRow(const std::vector<std::string>& items);
     void setHead(const std::vector<std::string>& head_items);
     void draw();
@@ -20,6 +22,7 @@ private:
     const unsigned int num_columns;
     std::vector<std::string> head;
     std::vector<std::string> rows;
+    std::tuple<int, int> dimensions;
 
 };
 
